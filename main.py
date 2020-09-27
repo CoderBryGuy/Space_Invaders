977f1f32fde61c85a56b9f460032d67d11e23e13
CoderBryGuy <bryan.dov.bergman@gmail.com>
2020-09-27 23:01:22 +0300

    working on player agility

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ screen = pygame.display.set_mode((800, 600))
 playerImg = pygame.image.load('/home/bryan/IdeaProjects/Python/Space_Invaders/space-invaders.png')
 playerX = 370
 playerY = 480
+playerX_velocity = 0
+playerY_velocity = 0
 
 
-def player():
+def player(x, y):
     screen.blit(playerImg, (playerX, playerY))
 
 
@@ -26,11 +28,22 @@ running = True
 # game loop
 while running:
     # RGB red, green, blue
-    screen.fill((255, 0, 0))
+    screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # if keystroke pressed check if left or right
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_velocity = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_velocity = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_velocity = 0
+
+    playerX += playerX_velocity
+    player(playerX, playerY)
     pygame.display.update()
